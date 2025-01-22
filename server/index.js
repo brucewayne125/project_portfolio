1fdaa38f9b5709560ad37f0aed48fdd6e5f9c6ee
brucewayne125 <snowdbat@gmail.com>
2025-01-22 19:06:31 +0300

    Setting up routes and models for testing

--- server/index.js
+++ server/index.js
@@ -23,3 +23,7 @@ app.get("/", (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const propertyRoutes = require("./routes/propertyRoutes");
+app.use("/api/properties", propertyRoutes);
+const vehicleRoutes = require("./routes/vehicleRoutes");
+app.use("/api/vehicles", vehicleRoutes);
---
+++ server/routes/vehicleRoutes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const Vehicle = require("../models/Vehicle");
+
+// Get All Vehicles
+router.get("/", async (req, res) => {
+  try {
+    const vehicles = await Vehicle.find();
+    res.json(vehicles);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Add a Vehicle
+router.post("/", async (req, res) => {
+  const { title, description, price, type } = req.body;
+
+  const newVehicle = new Vehicle({
+    title,
+    description,
+    price,
+    type,
+  });
+
+  try {
+    const savedVehicle = await newVehicle.save();
+    res.status(201).json(savedVehicle);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
